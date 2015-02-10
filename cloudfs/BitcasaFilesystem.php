d500c96401f5c6a09b31cead2eed0887572ef1ba
Gayan De Silva <gayan.d@calcey.com>
2015-02-10 14:53:08 +0530

    BitcasaFilesystem doc update

--- cloudfs/BitcasaFilesystem.php
+++ cloudfs/BitcasaFilesystem.php
@@ -13,15 +13,32 @@ require_once("BitcasaItems.php");
 require_once("BitcasaApi.php");
 require_once("BitcasaException.php");
 
-
+/**
+ * Represents the Bitcasa file system information.
+ */
 class Filesystem {
 
+	/**
+	 * The api instance.
+	 */
 	private $api;
 
+	/**
+	 * Initialize the api instance.
+	 *
+	 * @param $api The api instance.
+	 */
     public function __construct($api) {
 		$this->api = $api;
 	}
- 
+
+	/**
+	 * Retrieves the item array for a given directory.
+	 *
+	 * @param null $dir	The directory for which the items should be
+	 * 					retrieved for, if empty root items are retrieved.
+	 * @return The array of items at the given directory.
+	 */
     public function getList($dir = null) {
 		$path = "/";
 		if ($dir != null) {
@@ -41,8 +58,13 @@ class Filesystem {
 		}
 		return $lst;
 	}
- 
 
+	/**
+	 * Retrieves an item by the given path.
+	 *
+	 * @param $path	The file path.
+	 * @return An instance of the item of type Audio|Document|Photo|Video|File.
+	 */
 	public function getFile($path) {
 		if ($path == null) {
 			$path = "/";
@@ -61,8 +83,12 @@ class Filesystem {
 		return Item::make($res, $dir, $this);
 	}
 
-
-
+	/**
+	 * Retrieves a folder by the given path.
+	 *
+	 * @param $path	The folder path.
+	 * @return An instance of the item of type Folder.
+	 */
 	public function getFolder($path) {
 		$dir = null;
 		if ($path == null) {
@@ -82,7 +108,13 @@ class Filesystem {
 		return Item::make($res, $dir, $this);
 	}
 
-
+	/**
+	 * Delete multiple items from cloud storage.
+	 *
+	 * @param $items The items to be deleted.
+	 * @param bool $force The flag to force delete items from cloud storage.
+	 * @return The success/fail response of the delete operation.
+	 */
     public function delete($items, $force = false) {
 		if (!is_array($items)) {
 			$items = array($items);
@@ -100,7 +132,15 @@ class Filesystem {
 		return $res;
 	}
 
-
+	/**
+	 * Create a folder with supplied name under the given parent folders,
+	 * folder path.
+	 *
+	 * @param $parent Folder item under which the new folder should be created.
+	 * @param $name The name of the folder to be created.
+	 * @param string $exists Specifies the action to take if the folder already exists.
+	 * @return An instance of the newly created item of type Folder.
+	 */
 	public function create($parent, $name, $exists="overwrite") {
 		$parentItem = null;
 		if ($parent == null) {
@@ -116,7 +156,15 @@ class Filesystem {
 		return Item::make($r, $parentPath, $this);
 	}
 
-
+	/**
+	 * Moves multiple items to a specified destination.
+	 *
+	 * @param $items The items to be moved.
+	 * @param $destination Path to which the items should be moved to.
+	 * @param string $exists Specifies the action to take if the item already exists.
+	 * @return The success/fail response of the move operation
+	 * @throws InvalidArgument
+	 */
     public function move($items, $destination, $exists = "fail") {
 		assert_non_null($items, 1);
 		if (!is_string($destination)) {
@@ -137,7 +185,15 @@ class Filesystem {
 		}
 		return $res;
 	}
- 
+
+	/**
+	 * Copy multiple items to a specified destination.
+	 *
+	 * @param $items The items to be copied.
+	 * @param $destination Path to which the items should be copied to.
+	 * @param string $exists Specifies the action to take if the item already exists.
+	 * @return The success/fail response of the move operation
+	 */
     public function copy($items, $destination, $exists = "fail") {
 		if (!is_array($items)) {
 			$items = array($items);
@@ -154,7 +210,14 @@ class Filesystem {
 		}
 		return $res;
 	}
- 
+
+	/**
+	 * Update items on the cloud file system.
+	 *
+	 * @param $items The items to be updated.
+	 * @param string $conflict The action to take if a conflict occurs.
+	 * @return The success/fail response of the update operation.
+	 */
     public function save($items, $conflict = "fail") {
 		if (!is_array($items)) {
 			$items = array($items);
@@ -172,7 +235,14 @@ class Filesystem {
 		return $res;
 	}
 
-
+	/**
+	 * Upload a file on to the given path.
+	 *
+	 * @param $parent The parent folder path.
+	 * @param $path The upload file path.
+	 * @param string $exists The action to take if the item already exists.
+	 * @return An instance of the uploaded item.
+	 */
 	public function upload($parent, $path, $name = null, $exists = "overwrite") {
 		$parentpath = "/";
 		if (is_string($parent)) {
@@ -189,7 +259,13 @@ class Filesystem {
 		return Item::make($res['result'], $parentpath, $this);
 	}
 
- 
+	/**
+	 * Download an item from the cloud storage.
+	 *
+	 * @param $item The item to be downloaded.
+	 * @param null $file
+	 * @throws InvalidArgument
+	 */
 	public function download($item, $file = null) {
 		assert_non_null($item, 1);
 		$path = $item;
@@ -199,7 +275,14 @@ class Filesystem {
 		return $this->api->downloadFile($path, $file);
 	}
 
- 
+	/**
+	 * Restore a given set of items to the supplied destination.
+	 *
+	 * @param $items The items to be restored.
+	 * @param $destination The path the files are to be restored to
+	 * @param $exists The action to take if the item already exists.
+	 * @return The success/fail response of the restore operation.
+	 */
     public function restore($items, $destination, $exists) {
 		if (!is_array($items)) {
 			$items = array($items);
@@ -213,13 +296,19 @@ class Filesystem {
 		}
 		return $res;
 	}
- 
 
+	/**
+	 * Retrieves the file history of a given item.
+	 *
+	 * @param $item The item for which the file history needs to be retrieved.
+	 * @param int $start
+	 * @param int $stop
+	 * @return mixed
+	 */
     public function fileHistory($item, $start = -10, $stop = 0) {
 		return $this->api->fileHistory($item->path(), $start, $stop);
 	}
-
- 
+	
 }
  
 ?>
