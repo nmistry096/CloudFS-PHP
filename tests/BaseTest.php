2dc889af2dffebb8eecf2e735f748777d47995b6
Gayan De Silva <gayan.d@calcey.com>
2015-02-24 15:34:26 +0530

    BaseTest doc update

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,27 +1,55 @@
 <?php
 
+/**
+ * The base unit test.
+ */
 class BaseTest extends PHPUnit_Framework_TestCase {
 
-    const CLIENT_ID = 'k-O8NxC69MIlTe1vnCpemIMQe5B3LwCk73iGXGH-hwI';
-    const CLIENT_SECRET = 'Ka7F-bNlnR7ODiqhF4T2P6Cm8fWE0_yRKI6GaT_MTwXIEww7Pkpv-DnAHvFcukr6NR25wp5hxic7M4sy5X9ksA';
-    const END_POINT = 'm1qfb0fwlz.cloudfs.io';
-    const USERNAME = 'dilshan@calcey.com';
-    const PASSWORD = 'dilshan';
+    /**
+     * The bitcasa account related credential information.
+     */
+    const CLIENT_ID = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    const CLIENT_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+    const END_POINT = 'xxxxxxxxx.cloudfs.io';
+    const USERNAME = 'test@test.com';
+    const PASSWORD = 'xxxxxx';
 
+    /**
+     * The session variable of the unit test.
+     */
     private static $session;
 
+    /**
+     * Executed before executing all the tests.
+     * Sets the session up.
+     */
     public static function setUpBeforeClass() {
         self::$session = new Session(self::END_POINT, self::CLIENT_ID, self::CLIENT_SECRET);
     }
 
+    /**
+     * Executed after executing all the tests.
+     */
     public static function tearDownAfterClass() {
         self::$session = NULL;
     }
 
+    /**
+     * Retrieves the session object.
+     *
+     * @return The session object.
+     */
     protected function getSession() {
         return self::$session;
     }
 
+    /**
+     * Retrieves the path given an associative array.
+     *
+     * @param mixed $item The item path string or array.
+     * @param string $parent_path The parent item path.
+     * @return The processed path.
+     */
     protected function getPathFromAssociativeArray($item = null, $parent_path = null) {
         if ($parent_path == null) {
             $path = '/';
@@ -42,6 +70,13 @@ class BaseTest extends PHPUnit_Framework_TestCase {
         return $path;
     }
 
+    /**
+     * Retrieves the path of an item.
+     *
+     * @param mixed $item The item path string or an item instance.
+     * @param string $parent_path The parent path string.
+     * @return The processes path.
+     */
     protected function getPath($item = null, $parent_path = null) {
         if ($parent_path == null) {
             $path = '/';
@@ -62,6 +97,13 @@ class BaseTest extends PHPUnit_Framework_TestCase {
         return $path;
     }
 
+    /**
+     * Retrieves an item from a given item associative array.
+     *
+     * @param array $items The associative array containing items.
+     * @param string $item_name The name of the item to be retrieved.
+     * @return The retrieved item.
+     */
     protected function getItemFromAssociativeArray(array $items, $item_name) {
         $result = null;
         foreach($items['result']['items'] as $item) {
@@ -74,6 +116,13 @@ class BaseTest extends PHPUnit_Framework_TestCase {
         return $result;
     }
 
+    /**
+     * Retrieves an item from a given item array.
+     *
+     * @param array $items The array containing items.
+     * @param $item_name The name of the item to be retrieved.
+     * @return The retrieved item.
+     */
     protected function getItemFromIndexArray(array $items, $item_name) {
         $result = null;
         foreach($items as $item) {
