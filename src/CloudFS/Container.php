35dc086868611829154cee70bc1f94b26d0fa241
Gayan De Silva <gayan.d@calcey.com>
2015-03-11 14:37:27 +0530

    SDK Refactor

--- src/CloudFS/Container.php
+++ src/CloudFS/Container.php
@@ -29,27 +29,4 @@ class Container extends Item {
         return $this->api()->getList($this->getPath());
     }
 
-    /**
-     * Creates a folder item under this item with the supplied name.
-     *
-     * @param string $name The name of the folder being created.
-     * @param string $exists The action to take if the folder already exists.
-     * @return Instance of the newly created folder.
-     */
-    public function create($name, $exists="overwrite") {
-        return $this->api()->create($this, $name, $exists);
-    }
-
-    /**
-     * Uploads a file under this item.
-     *
-     * @param string $path The path of the file to be uploaded.
-     * @param string $name The name of the file.
-     * @param string $exists The action to take if the file already exists.
-     * @return An instance of the uploaded item.
-     */
-    public function upload($path, $name = null, $exists='fail') {
-        return $this->api()->upload($this, $path, $name, $exists);
-    }
-
 }
--- src/CloudFS/Folder.php
+++ src/CloudFS/Folder.php
@@ -20,4 +20,26 @@ class Folder extends Container {
         parent::__construct($api);
     }
 
+    /**
+     * Creates a folder item under this item with the supplied name.
+     *
+     * @param string $name The name of the folder being created.
+     * @param string $exists The action to take if the folder already exists.
+     * @return Instance of the newly created folder.
+     */
+    public function createFolder($name, $exists="overwrite") {
+        return $this->api()->create($this, $name, $exists);
+    }
+
+    /**
+     * Uploads a file under this item.
+     *
+     * @param string $path The path of the file to be uploaded.
+     * @param string $name The name of the file.
+     * @param string $exists The action to take if the file already exists.
+     * @return An instance of the uploaded item.
+     */
+    public function upload($path, $name = null, $exists='fail') {
+        return $this->api()->upload($this, $path, $name, $exists);
+    }
 }
